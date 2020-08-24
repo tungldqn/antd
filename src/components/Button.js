aa57c1cc084ec2a2101c9e3aa19ce8e29ae19748
Le Duy Tung <tungld@i.hivetech.vn>
2020-08-24 18:19:28 +0700

    init

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'antd';
+
+function TestButton() {
+  return (
+    <Button type="primary">Primary Button</Button>
+  )
+}
+
+export default TestButton;
